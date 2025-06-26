4dc6408aed8d34e34b5e6267326a31a67659282b
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-06-26 09:31:22 +0300

    inital

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './components/NavBar';
+
+export const App = () => {
+  return <>
+  <Navbar />
+  </>
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => <>
+<nav className="py-5 px-5 flex w-[97vw] h-[60vh] items-center justify-center">
+    <a href="#" className="gradient-outline p-6 aspect-square flex justify-center items-center rounded-full before:rounded-full backdrop-blur-xl shadow-[inset_1px_3px_4px_rgba(255,255,255,0.6)]"><img src="/logo.svg" alt="K" width={30} /></a>
+</nav>
+</>;
+
+
+export default Navbar
