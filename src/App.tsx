b3f036049fff519d362fdbe1232a352b44478c27
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-01 20:55:37 +0300

    Created responsive About section!

--- src/App.tsx
+++ src/App.tsx
@@ -1674,7 +1674,226 @@ export const App = () => {
           ]}
         />
       </section>
+      <section
+        id="about"
+        className="backdrop-sepia-100 flex justify-center items-center min-h-[100vh] text-white/70 museomoderno-400"
+      >
+        <div className="content flex flex-col gap-15 p-10 2xl:px-80 xl:px-50 lg:px-25 lg:flex-row lg:gap-5">
+          <div className="personal-info flex flex-col gap-3 flex-[2]">
+            <h1 className="text-2xl font-bold">Hi, I am Khaled Muhammad</h1>
+            <h3>
+              Senior 3 Student,{" "}
+              <i className="relative before:absolute before:bg-purple-700/50 before:h-full before:w-full before:-z-10 animate-ud">
+                We School
+              </i>
+            </h3>
+            <h3>Alexandria, Egypt 🇪🇬</h3>
+            <div className="flex flex-col gap-6 mt-5">
+              <div className="secondary-info bg-gradient-to-bl from-white/40 to-white/30 py-3 rounded-2xl flex flex-col gap-3 relative gradient-outline before:rounded-2xl">
+                <h1 className="mx-3 text-3xl">Full Stack & Mobile Developer</h1>
+                <p className="mx-3">
+                  Focus areas: Mobile apps, AI-backed tools, UI/UX,
+                  problem-solving
+                </p>
+              </div>
+              <div className="main-skills-group flex gap-5 justify-center my-5">
+                {skills
+                  .filter((sk) =>
+                    ["React", "Flutter", "Django", "Python"].includes(sk.title)
+                  )
+                  .map((sk) => (
+                    <div className="w-15 h-15">{sk.icon}</div>
+                  ))}
+              </div>
+            </div>
+            <div className="ring-2 ring-white/50 rounded-2xl p-2">
+              <h1 className="mx-3 text-3xl text-center">Values</h1>
+              <p className="text-center">Curiosity, growth & impact</p>
+            </div>
+            <div className="flex justify-center mt-6">
+              <GlassyButton text="Read More About Me" />
+            </div>
+          </div>
+          <div className="flex-[1.8] flex flex-col justify-center items-center">
+            <div className="two-layer-img [grid-area:myPic] relative">
+              <div className="back-layer absolute inset-0 bg-gradient-to-br from-white/60 rounded-md -translate-5 -z-10 gradient-outline before:rounded-md backdrop-invert-100 overflow-hidden">
+                <div className="circle absolute w-[50%] bg-gradient-to-b from-white/20 to-white/60 aspect-square rounded-full blur-sm -translate-1/2"></div>
+              </div>
+              <img
+                src="https://ca.slack-edge.com/T0266FRGM-U08QKDF6F9B-edcd82c42589-2048"
+                alt="Me"
+                className="rounded-md aspect-[2/3] object-cover w-85 animate-float"
+              />
+            </div>
+            <div className="tag-line [grid-area:tagLine] px-8 mt-5">
+              <p>
+                Engineering Ideas. Hacking Problems. Designing with Purpose.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center flex-[2]">
+            <div className="main-info bg-gradient-to-bl from-white/40 to-white/30 py-3 rounded-2xl flex flex-col gap-3 relative gradient-outline before:rounded-2xl">
+              <h1 className="mx-3 text-3xl">About Me</h1>
+              <h3 className="label bg-green-500/20">Open To Work</h3>
+              <hr />
+              <p className="mx-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                temporibus sequi, at velit, repellendus ratione tempore alias
+                facere vero, beatae saepe maiores harum necessitatibus tenetur.
+                Nemo impedit at dolor consequatur, esse cupiditate corporis eos
+                placeat, tempora, assumenda officia in maiores! Facilis deserunt
+                repellendus recusandae omnis vitae! Facilis harum autem cumque?
+              </p>
+            </div>
+            <div className="flex gap-8 items-center">
+              <a href="#">
+                <div className="bubble">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 48 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <defs>
+                      <linearGradient
+                        id="whiteGradient"
+                        x1="0"
+                        y1="0"
+                        x2="48"
+                        y2="48"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="rgba(255,255,255,0.6)" offset="0%" />
+                        <stop stopColor="rgba(255,255,255,0.4)" offset="100%" />
+                      </linearGradient>
+                    </defs>
 
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M24.0432 0.179932C10.8147 0.179932 0.0876465 11.0878 0.0876465 24.5445C0.0876465 35.3096 6.95165 44.4426 16.4699 47.6643C17.6671 47.8899 18.1067 47.1358 18.1067 46.4922C18.1067 45.9112 18.0845 43.9919 18.0742 41.956C11.4097 43.4299 10.0034 39.0812 10.0034 39.0812C8.9137 36.265 7.34358 35.5161 7.34358 35.5161C5.17009 34.0039 7.50742 34.035 7.50742 34.035C9.91297 34.2065 11.1796 36.5458 11.1796 36.5458C13.3162 40.2707 16.7837 39.1938 18.1507 38.5712C18.3657 36.9969 18.9866 35.9212 19.6716 35.3132C14.3508 34.6971 8.7574 32.6079 8.7574 23.2719C8.7574 20.6118 9.6932 18.4383 11.2256 16.732C10.9769 16.1179 10.1569 13.6402 11.4577 10.2841C11.4577 10.2841 13.4693 9.62928 18.0472 12.7816C19.9581 12.2418 22.0074 11.971 24.0432 11.9618C26.0791 11.971 28.13 12.2418 30.0444 12.7816C34.6167 9.62928 36.6256 10.2841 36.6256 10.2841C37.9295 13.6402 37.1091 16.1179 36.8604 16.732C38.3964 18.4383 39.3259 20.6118 39.3259 23.2719C39.3259 32.6301 33.7218 34.6906 28.3874 35.2938C29.2467 36.0499 30.0123 37.5327 30.0123 39.8059C30.0123 43.0655 29.9845 45.6893 29.9845 46.4922C29.9845 47.1406 30.4157 47.9003 31.63 47.6611C41.1431 44.4357 47.9984 35.3059 47.9984 24.5445C47.9984 11.0878 37.273 0.179932 24.0432 0.179932Z"
+                      fill="url(#whiteGradient)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M9.16084 35.1623C9.10808 35.2837 8.92084 35.3196 8.75026 35.2365C8.57651 35.157 8.47892 34.992 8.53525 34.8706C8.58682 34.7459 8.77446 34.7116 8.94781 34.7943C9.12196 34.8742 9.22113 35.0408 9.16084 35.1623Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M10.1312 36.263C10.0169 36.3707 9.79356 36.3207 9.64203 36.1504C9.48533 35.9805 9.45598 35.7534 9.57181 35.644C9.68963 35.5363 9.90622 35.5867 10.0633 35.7566C10.22 35.9285 10.2506 36.154 10.1312 36.263Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M11.0757 37.6662C10.9289 37.7699 10.6889 37.6727 10.5405 37.456C10.3938 37.2394 10.3938 36.9795 10.5437 36.8754C10.6925 36.7713 10.9289 36.8649 11.0793 37.08C11.2256 37.2999 11.2256 37.5601 11.0757 37.6662Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M12.3697 39.0219C12.2384 39.1692 11.9587 39.1296 11.754 38.9287C11.5446 38.7322 11.4863 38.4534 11.618 38.3062C11.7509 38.1585 12.0321 38.2 12.2384 38.3994C12.4463 38.5954 12.5097 38.8763 12.3697 39.0219Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M14.1548 39.8091C14.0969 39.9999 13.8275 40.0867 13.5562 40.0056C13.2853 39.9221 13.1079 39.6985 13.1627 39.5057C13.219 39.3136 13.4896 39.2232 13.7629 39.31C14.0334 39.3931 14.2112 39.615 14.1548 39.8091Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M16.1153 39.9552C16.122 40.1561 15.8919 40.3227 15.6071 40.3259C15.3207 40.3328 15.089 40.1702 15.0859 39.9725C15.0859 39.7696 15.3108 39.6045 15.5972 39.5997C15.882 39.594 16.1153 39.7554 16.1153 39.9552Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M17.9397 39.6392C17.9738 39.8353 17.7758 40.0367 17.493 40.0899C17.2149 40.142 16.9575 40.0209 16.9222 39.8264C16.8876 39.6255 17.0892 39.4242 17.3669 39.3721C17.6501 39.3221 17.9036 39.4399 17.9397 39.6392Z"
+                      fill="rgba(255,255,255,1)"
+                    />
+                  </svg>
+                </div>
+              </a>
+              <GlassyButton text="Open CV" className="" />
+              <a href="#">
+                <div className="bubble">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 48 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <defs>
+                      <linearGradient
+                        id="#blueGradient"
+                        x1="0"
+                        y1="0"
+                        x2="48"
+                        y2="48"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="rgba(255,255,255,0.6)" offset="0%" />
+                        <stop stopColor="rgba(255,255,255,0.4)" offset="100%" />
+                      </linearGradient>
+                    </defs>
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24ZM16.9605 19.8778H11.5216V36.2196H16.9605V19.8778ZM17.3188 14.8227C17.2835 13.2204 16.1377 12 14.277 12C12.4164 12 11.2 13.2204 11.2 14.8227C11.2 16.3918 12.3805 17.6473 14.2064 17.6473H14.2412C16.1377 17.6473 17.3188 16.3918 17.3188 14.8227ZM36.5754 26.8497C36.5754 21.8303 33.8922 19.4941 30.3131 19.4941C27.4254 19.4941 26.1326 21.0802 25.4107 22.1929V19.8783H19.9711C20.0428 21.4117 19.9711 36.22 19.9711 36.22H25.4107V27.0934C25.4107 26.605 25.446 26.1178 25.5898 25.7681C25.9829 24.7924 26.8779 23.7822 28.3805 23.7822C30.3494 23.7822 31.1365 25.2807 31.1365 27.4767V36.2196H36.5752L36.5754 26.8497Z"
+                      fill="url(##blueGradient)"
+                    />
+                  </svg>
+                </div>
+              </a>
+            </div>
+            <div className="contin-links flex gap-4">
+              {Array.from({ length: 5 }, (value, index) => index).map(() => (
+                <a href="#">
+                  <div className="bubble">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 48 48"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <defs>
+                        <linearGradient
+                          id="#blueGradient"
+                          x1="0"
+                          y1="0"
+                          x2="48"
+                          y2="48"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stopColor="rgba(255,255,255,0.6)" offset="0%" />
+                          <stop
+                            stopColor="rgba(255,255,255,0.4)"
+                            offset="100%"
+                          />
+                        </linearGradient>
+                      </defs>
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24ZM16.9605 19.8778H11.5216V36.2196H16.9605V19.8778ZM17.3188 14.8227C17.2835 13.2204 16.1377 12 14.277 12C12.4164 12 11.2 13.2204 11.2 14.8227C11.2 16.3918 12.3805 17.6473 14.2064 17.6473H14.2412C16.1377 17.6473 17.3188 16.3918 17.3188 14.8227ZM36.5754 26.8497C36.5754 21.8303 33.8922 19.4941 30.3131 19.4941C27.4254 19.4941 26.1326 21.0802 25.4107 22.1929V19.8783H19.9711C20.0428 21.4117 19.9711 36.22 19.9711 36.22H25.4107V27.0934C25.4107 26.605 25.446 26.1178 25.5898 25.7681C25.9829 24.7924 26.8779 23.7822 28.3805 23.7822C30.3494 23.7822 31.1365 25.2807 31.1365 27.4767V36.2196H36.5752L36.5754 26.8497Z"
+                        fill="url(##blueGradient)"
+                      />
+                    </svg>
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
