3fd83fe0e8ef859de042db1bf856f8a98550fc53
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-04 10:18:26 +0300

    Add new routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import "./components/AnimatedMeshBg.css";
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ContactRoute from './routes/Contact.tsx';
+import Layout from './components/Layout.tsx';
+import CertificatesRoute from './routes/Certificates.tsx';
+import CertificateRoute from './routes/Certificate.tsx';
+import FourOFour from './routes/FourOFour.tsx';
+import { ToastContainer } from 'react-toastify';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="book" element={<ContactRoute />} />
+          <Route path="certs" element={<CertificatesRoute />}>
+            <Route path=":id" element={<CertificateRoute />} />
+          </Route>
+          <Route path="*" element={<FourOFour />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <ToastContainer />
   </StrictMode>
 )
